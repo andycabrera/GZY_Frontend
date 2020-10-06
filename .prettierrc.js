f1c1e2a050c3dd096dd1ae69a2312a0658e52cc0
mobzantrecu <nicolas.rodriguezlb@gmail.com>
2020-10-06 09:56:48 -0300

    next + lints

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "semi": false,
+    "tabWidth": 4,
+    "printWidth": 80,
+    "singleQuote": true,
+    "trailingComma": "none",
+    "trailingComma": 'es5',
+    "useTabs": false,
+    "bracketSpacing": true,
+    "jsxBracketSameLine": false,
+}
