65f564f01d4250ef8506ac4e164f308dffdcd3dd
mobzantrecu <nicolas.rodriguezlb@gmail.com>
2020-10-08 01:42:31 -0300

    EsLint motherFucker

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,23 +1,13 @@
-import { default as React, default as React, FC, ReactChildren } from 'react'
+import React, { FC, ReactChildren } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
   min-height = 100vh;
 `
 
-
-export default function Layout() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-
 interface LayoutProps {
     children: ReactChildren
 }
 export const Layout: FC<LayoutProps> = ({ children }) => {
-    return (<img src=""></img>)
+    return <Container />
 }
