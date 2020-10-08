a96df934234a91db81cef1a2f49d9f63ace9d356
mobzantrecu <nicolas.rodriguezlb@gmail.com>
2020-10-08 00:40:42 -0300

    pages

---
+++ pages/_app.tsx
@@ -0,0 +1,38 @@
+import React, { ReactElement } from 'react'
+import Head from 'next/head'
+import { ReactQueryConfigProvider } from 'react-query'
+
+// TODO: Que es _app de next
+export default function MyApp(props): ReactElement {
+    const { Component, pageProps } = props
+
+    React.useEffect(() => {
+        // eslint-disable-next-line no-undef
+        const jssStyles = document.querySelector('#jss-server-side')
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles)
+        }
+    }, [])
+
+    return (
+        <>
+            <Head>
+                <title>Gzi shop</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <ReactQueryConfigProvider
+                config={{
+                    queries: {
+                        retry: false,
+                        refetchOnWindowFocus: false,
+                    },
+                }}
+            >
+                <Component {...pageProps} />
+            </ReactQueryConfigProvider>
+        </>
+    )
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
+// TODO: Que es Docomento de next
+class MyDocument extends Document {
+    render(): JSX.Element {
+        return (
+            <Html lang="es">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
